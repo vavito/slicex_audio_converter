fc8d9e2d41c99a82da0032dc885e45e717c0730b
vavito <joaovictor2126@gmail.com>
2026-05-19 15:25:03 -0300

    primeiro commit

---
+++ backend/src/main/java/com/slicex/audioconverter/AudioconverterApplication.java
@@ -0,0 +1,13 @@
+package com.slicex.audioconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioconverterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/slicex/audioconverter/AudioconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slicex.audioconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
