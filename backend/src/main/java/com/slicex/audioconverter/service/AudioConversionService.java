261260d30953c72b1525e6238549f316d4213077
vavito <joaovictor2126@gmail.com>
2026-05-23 20:05:40 -0300

    feat: parâmetro adicionado para encaixar com método de FmmpegAudioConverter

--- backend/src/main/java/com/slicex/audioconverter/service/AudioConversionService.java
+++ backend/src/main/java/com/slicex/audioconverter/service/AudioConversionService.java
@@ -39,7 +39,8 @@ public class AudioConversionService {
 
             File outputFile = ffmpegAudioConverter.convert(
                     inputFile,
-                    audioConversion.getFormatoPosConversao()
+                    audioConversion.getFormatoPosConversao(),
+                    audioConversion.getNomeDoArquivoOriginal()
             );
 
             audioConversion.concluir();
